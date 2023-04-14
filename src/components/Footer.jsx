060c1e4a40a3a7f145e73f033365df4fcecf7179
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2023-04-15 02:15:13 +0500

    Keeper App v4.1 - Bug Fix
    Update README & No more anonymous function in Footer.jsx and Header.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function Footer() {
     const date = new Date;
     const year = date.getFullYear();
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import HighlightIcon from '@mui/icons-material/Highlight';
 
-export default function () {
+export default function Header() {
     return (
         <header>
             <h1><HighlightIcon />Keeper</h1>
